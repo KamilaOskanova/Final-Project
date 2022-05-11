95389196e53d0306e47dd37ca9a6897d67bcaef2
«Kamila» <koskanova@outlook.com>
2022-05-11 14:48:47 +0300

    Added the Final Work that outputs three or less symbols strings

---
+++ FinalWork/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести 
+// с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется
+// пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.Clear();
+
+void ThreeOrLessSymbolString(string[] massiv)
+{
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        string value = massiv[i];
+        if (value.Length <= 3)
+        {
+            Console.Write(massiv[i] + " ");
+        }
+    }
+}
+
+string[] array = { "Hello", "1", "two", ":-)", "Kazan", "Denmark", "123" };
+ThreeOrLessSymbolString(array);
